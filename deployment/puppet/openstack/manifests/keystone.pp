3a919e428c1d36d3479234b65ea8c02e9ba4f2a7
Sergiy Lystopad <slystopad@mirantis.com>
2014-11-12 15:53:48 +0200

    Change default keystone username for services to 'servicename_sys' TODO: should be awailable as UI parameters

--- deployment/puppet/openstack/manifests/keystone.pp
+++ deployment/puppet/openstack/manifests/keystone.pp
@@ -99,6 +99,13 @@ class openstack::keystone (
   $max_overflow                = '30',
   $max_retries                 = '-1',
   $use_ldap                    = $::fuel_settings['keystone_ldap']['use_ldap'],
+  $auth_name_suffix            = '_sys'
+  ## neutron user was changed int sanitise_quantum_config function 
+  #$neutron_auth_name
+  $ceilometer_auth_name        = "ceilometer${auth_name_suffix}",
+  $cinder_auth_name            = 'cinder${auth_name_suffix}',
+  $nova_auth_name              = 'nova${auth_name_suffix}',
+  $glance_auth_name            = 'glance${auth_name_suffix}',
 ) {
 
   # Install and configure Keystone
@@ -250,6 +257,7 @@ class openstack::keystone (
         public_address   => $glance_public_real,
         admin_address    => $glance_admin_real,
         internal_address => $glance_internal_real,
+        auth_name        => $glance_auth_name,
       }
       Exec <| title == 'keystone-manage db_sync' |> -> Class['glance::keystone::auth']
     }
@@ -262,6 +270,7 @@ class openstack::keystone (
         admin_address    => $nova_admin_real,
         internal_address => $nova_internal_real,
         cinder            => $cinder,
+        auth_name         => $nova_auth_name,
       }
       Exec <| title == 'keystone-manage db_sync' |> -> Class['nova::keystone::auth']
     }
@@ -273,6 +282,7 @@ class openstack::keystone (
         public_address   => $cinder_public_real,
         admin_address    => $cinder_admin_real,
         internal_address => $cinder_internal_real,
+        auth_name        => $cinder_auth_name,
       }
      Exec <| title == 'keystone-manage db_sync' |> -> Class['cinder::keystone::auth']
     }
@@ -291,6 +301,7 @@ class openstack::keystone (
         public_address   => $ceilometer_public_real,
         admin_address    => $ceilometer_admin_real,
         internal_address => $ceilometer_internal_real,
+        auth_name        => $ceilometer_auth_name,
       }
       Exec <| title == 'keystone-manage db_sync' |> -> Class['ceilometer::keystone::auth']
     }
